b431d5cc53f4ed2e0b5af55b7f71ad7fc3b35723
jjinnm4real <79997601+jjinnm4real@users.noreply.github.com>
2021-07-20 14:07:49 -0600

    Update create_json.py

--- Test3/create_json.py
+++ Test3/create_json.py
@@ -1,22 +1,11 @@
-import json
+from behave import given, when, then
 
-data = {}
-data['people'] = []
-data['people'].append({
-    'name': 'Scott',
-    'website': 'stackabuse.com',
-    'from': 'Nebraska'
-})
-data['people'].append({
-    'name': 'Larry',
-    'website': 'google.com',
-    'from': 'Michigan'
-})
-data['people'].append({
-    'name': 'Tim',
-    'website': 'apple.com',
-    'from': 'Alabama'
-})
-
-with open('data.json', 'w') as outfile:
-    json.dump(data, outfile)
+@given('we have behave installed')
+def step_impl(context):
+    pass
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False
